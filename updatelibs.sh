1d6b7eb26ca8eb8c80fc9df0cbf32e4409952848
KirbySaysHi <kirbysayshi@gmail.com>
2012-01-08 20:34:15 -0500

    now using update-able rAF.js, via https://twitter.com/paul_irish/status/156119515485306880

--- updatelibs.sh
+++ updatelibs.sh
@@ -5,6 +5,7 @@ FILES[0]=https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
 FILES[1]=http://dat-gui.googlecode.com/git/build/dat.gui.js
 FILES[2]=https://raw.github.com/toji/gl-matrix/master/gl-matrix.js
 FILES[3]=https://raw.github.com/mrdoob/stats.js/master/src/Stats.js
+FILES[4]=https://raw.github.com/gist/1579671/rAF.js
 
 pushd ./lib
 
